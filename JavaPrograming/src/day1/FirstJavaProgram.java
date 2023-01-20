4febc5970ad61d2358cad68057127c08f49dac02
HP <HP@SaurabhChoudhari>
2023-01-20 16:09:48 +0530

    first commit

---
+++ JavaPrograming/src/day1/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class FirstJavaProgram
+{
+	public static void main(String args [])
+	{
+		//code
+		System.out.println ("Wellcome to java Programing");
+		System.out.println (10+20);
+	}
+}
---
+++ JavaPrograming/src/day1/SecondJavaProgram.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class SecondJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("wellcome");
+		System.out.println("Selenium Automation");
+		System.out.println("100");
+		System.out.println(100+200);
+	}
+
+}
